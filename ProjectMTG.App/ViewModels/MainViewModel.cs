c0e3ba454ddfd87fb5472f2d6d9fefb19ea7777a
bj0rnar <bj0rnar@hotmail.com>
2019-04-28 13:39:39 +0200

    Can import data from EF in listview!

--- ProjectMTG.App/ViewModels/MainViewModel.cs
+++ ProjectMTG.App/ViewModels/MainViewModel.cs
@@ -1,13 +1,39 @@
 ﻿using System;
-
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using ProjectMTG.App.DataAccess;
 using ProjectMTG.App.Helpers;
+using ProjectMTG.Model;
 
 namespace ProjectMTG.App.ViewModels
 {
     public class MainViewModel : Observable
     {
+        private ObservableCollection<Card> ObservableCards = new ObservableCollection<Card>();
+        public ObservableCollection<Card> GetObservableCards => this.ObservableCards;
+
+        private ObservableCollection<Card> ObservableDeck = new ObservableCollection<Card>();
+        public ObservableCollection<Card> GetObservableDeck => this.ObservableDeck;
+        private Cards cardsDataAccess = new Cards();
+
         public MainViewModel()
         {
+            this.ObservableCards.Add(new Card() { name = "CardTest"});
+        }
+
+
+
+
+        //Denna metoda funke ikkje, spør um hjelp.
+        internal async Task LoadCardsAsync()
+        {
+            var cards = await cardsDataAccess.GetCardsAsync();
+            foreach (Card card in cards)
+            {
+                ObservableCards.Add(card);
+            }
         }
     }
 }
--- ProjectMTG.App/Views/MainPage.xaml.cs
+++ ProjectMTG.App/Views/MainPage.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using Windows.UI.Xaml;
 using ProjectMTG.App.ViewModels;
 
 using Windows.UI.Xaml.Controls;
+using ProjectMTG.Model;
 
 namespace ProjectMTG.App.Views
 {
@@ -13,6 +15,37 @@ namespace ProjectMTG.App.Views
         public MainPage()
         {
             InitializeComponent();
+
+            //DeckListView.ItemsSource = 
+
+            Loaded += MainPage_Loaded;
         }
+
+        private async void MainPage_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            await ViewModel.LoadCardsAsync();
+        }
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            /*
+            foreach (ListViewItem selectedCard in CardListView.SelectedItems)
+            {
+                DeckListView.Items.Add((ListViewItem)selectedCard);
+            }
+            */
+        }
+
+        /*
+        private static void AddCardsToDeck(ListView cardView, ListView deckView)
+        {
+            foreach (Card selectedItem in cardView.SelectedItems)
+            {
+                deckView.Items.Add((Card)selectedItem);
+            }
+        }
+        */
     }
 }
