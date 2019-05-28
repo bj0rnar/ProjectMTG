04311eb4bea6a6f0e29a5970fdf75c0941e075a4
bj0rnar <bj0rnar@hotmail.com>
2019-05-28 19:49:29 +0200

    Grisåt simulator

--- ProjectMTG.App/ViewModels/SimulationViewModel.cs
+++ ProjectMTG.App/ViewModels/SimulationViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,12 +13,10 @@ using ProjectMTG.Model;
 
 namespace ProjectMTG.App.ViewModels
 {
-    public class SimulationViewModel : Observable
+    public class SimulationViewModel : Observable, INotifyPropertyChanged
     {
 
         private User user = ShellViewModel.LoggedInUser;
-
-        private static Random randomGenerator = new Random();
 
         private Decks decksDataAccess = new Decks();
 
@@ -30,10 +29,22 @@ namespace ProjectMTG.App.ViewModels
         private ObservableCollection<BitmapImage> DisplayImages = new ObservableCollection<BitmapImage>();
         public ObservableCollection<BitmapImage> GetDisplayImages => this.DisplayImages;
 
+        public double ZeroLand { get; set; }
+        public double OneLand { get; set; }
+        public double TwoLand { get; set; }
+        public double ThreeLand { get; set; }
+        public double FourLand { get; set; }
+        public double FiveLand { get; set; }
+        public double SixLand { get; set; }
+        public double SevenLand { get; set; }
+
+
+        public ICommand SimulateDrawCommand { get; }
+
 
         public SimulationViewModel()
         {
-            
+            SimulateDrawCommand = new RelayCommand<Deck>(SimulateDraw, param => param != null);
         }
 
         internal async Task LoadDecks()
@@ -44,6 +55,79 @@ namespace ProjectMTG.App.ViewModels
             {
                 ComboBoxDecks.Add(deck);
             }
+        }
+
+        private void SimulateDraw(Deck deck)
+        {
+            ZeroLand = 0;
+            OneLand = 0;
+            TwoLand = 0;
+            ThreeLand = 0;
+            FourLand = 0;
+            FiveLand = 0;
+            SixLand = 0;
+            SevenLand = 0;
+
+            int landPerDraw = 0;
+
+            for (var i = 0; i < 100; i++)
+            {
+                var randomizedList = deck.Cards.TakeRandom(7);
+
+                foreach (var card in randomizedList)
+                {
+                    if (card.types.Contains("Land"))
+                    {
+                        landPerDraw++;
+                    }
+                }
+
+                switch (landPerDraw)
+                {
+                    case 0:
+                        ZeroLand++;
+                        break;
+                    case 1:
+                        OneLand++;
+                        break;
+                    case 2:
+                        TwoLand++;
+                        break;
+                    case 3:
+                        ThreeLand++;
+                        break;
+                    case 4:
+                        FourLand++;
+                        break;
+                    case 5:
+                        FiveLand++;
+                        break;
+                    case 6:
+                        SixLand++;
+                        break;
+                    case 7:
+                        SevenLand++;
+                        break;
+                    default: //Default means more than seven
+                        SevenLand++;
+                        break;
+                }
+
+                landPerDraw = 0;
+
+            }
+
+            Debug.WriteLine("Zeroland" + ZeroLand);
+            Debug.WriteLine("Oneland % " + OneLand);
+            Debug.WriteLine("Twoland % " + TwoLand);
+            Debug.WriteLine("Threeland % " + ThreeLand);
+            Debug.WriteLine("Fourland % " + FourLand);
+            Debug.WriteLine("Fiveland % " + FiveLand);
+            Debug.WriteLine("Sixland % " + SixLand);
+            Debug.WriteLine("Sevenland % " + SevenLand);
+
+
+
         }
 
         public async void DrawNewHand(Deck selectedDeck, int draw)
