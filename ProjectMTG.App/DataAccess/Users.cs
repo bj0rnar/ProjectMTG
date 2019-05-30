15d6793ca17494ac35147e501d0bde18d9539cc8
bj0rnar <bj0rnar@hotmail.com>
2019-05-30 07:30:18 +0200

    changepassword

--- ProjectMTG.App/DataAccess/Users.cs
+++ ProjectMTG.App/DataAccess/Users.cs
@@ -11,12 +11,12 @@ namespace ProjectMTG.App.DataAccess
 {
     public class Users
     {
-        static Uri UserUri = new Uri("http://localhost:61254/api/users");
+        static Uri _userUri = new Uri("http://localhost:61254/api/users");
         readonly HttpClient _httpClient = new HttpClient();
 
         public async Task<User[]> GetUsersAsync()
         {
-            var clientResult = await _httpClient.GetAsync(UserUri);
+            var clientResult = await _httpClient.GetAsync(_userUri);
             var jsonData = await clientResult.Content.ReadAsStringAsync();
             User[] users = JsonConvert.DeserializeObject<User[]>(jsonData);
 
@@ -26,8 +26,15 @@ namespace ProjectMTG.App.DataAccess
         internal async Task<bool> AddUser(User user)
         {
             string json = JsonConvert.SerializeObject(user);
-            HttpResponseMessage result = await _httpClient.PostAsync(UserUri, new StringContent(json, Encoding.UTF8, "application/json"));
+            HttpResponseMessage result = await _httpClient.PostAsync(_userUri, new StringContent(json, Encoding.UTF8, "application/json"));
 
+            return result.IsSuccessStatusCode;
+        }
+
+        internal async Task<bool> ChangePassword(User user)
+        {
+            var json = JsonConvert.SerializeObject(user);
+            HttpResponseMessage result = await _httpClient.PutAsync(new Uri(_userUri, "users/" + user.UserId), new StringContent(json, Encoding.UTF8, "application/json"));
             return result.IsSuccessStatusCode;
         }
     }
---
+++ ProjectMTG.App/ViewModels/UserSettingsViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Input;
+using ProjectMTG.App.DataAccess;
+using ProjectMTG.App.Helpers;
+using ProjectMTG.App.Views;
+using ProjectMTG.Model;
+
+namespace ProjectMTG.App.ViewModels
+{
+    public class UserSettingsViewModel : Observable
+    {
+        private User _user = ShellViewModel.LoggedInUser;
+        public Users _usersDataAccess = new Users();
+
+
+        public ICommand ChangePassword { get; set; }
+        public ICommand DeleteUser { get; set; }
+
+        public UserSettingsViewModel()
+        {
+            ChangePassword = new RelayCommand(ChangeUserPassword);
+        }
+
+        private async void ChangeUserPassword()
+        {
+            ChangePasswordDialog pwDialog = new ChangePasswordDialog();
+            await pwDialog.ShowAsync();
+
+            if (pwDialog.Content != null)
+            {
+                string pw = (string) pwDialog.Content;
+                _user.Password = pw;
+                if (await _usersDataAccess.ChangePassword(_user))
+                {
+                    Debug.WriteLine("Change password!");
+                }
+            }
+
+        }
+    }
+}
